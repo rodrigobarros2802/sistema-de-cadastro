8e2d5ccac56ba9b875dde98a78445577b58bd006
Rodrigo Barros <rodrigobarros2802@hotmail.com>
2022-06-05 11:04:14 -0300

    Voltando atrás para a alteração de paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o Sistema de Gerencimaneto de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //Mais um console log de test:
 console.log("O paulo fez esse commi sem você ver na aula!")
-console.log("Enviando para o GitHub")
