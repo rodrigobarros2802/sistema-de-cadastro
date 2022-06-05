e79cb728668783ee07f8c3f6f8df094b84d118d5
Rodrigo Barros <rodrigobarros2802@hotmail.com>
2022-06-05 10:46:19 -0300

    Alterando o APP JS

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o Sistema de Gerencimaneto de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+//Mais um console log de test:
+console.log("O paulo fez esse commi sem você ver na aula!")
